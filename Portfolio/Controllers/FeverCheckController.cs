105a42167c8a94ab989d8f5d7ecc49c770f311fe
GreenMutant <lothius@hotmail.com>
2022-05-09 10:34:45 +0200

    Add project files.

---
+++ Portfolio/Controllers/FeverCheckController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Portfolio.Models;
+
+namespace Portfolio.Controllers
+{
+    public class FeverCheckController : Controller
+    {
+        public IActionResult GetInfo()
+        {
+            ViewBag.Message = FeverCheckModel.WriteMessage();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult GetInfo(string temp)
+        {
+            FeverCheckModel model = new FeverCheckModel();
+            ViewBag.Message = model.CheckFever(temp);
+            return View();
+        }
+    }
+}
---
+++ Portfolio/Controllers/GuessingGameController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Portfolio.Models;
+
+namespace Portfolio.Controllers
+{
+    public class GuessingGameController : Controller
+    {
+        
+
+       
+       
+
+        
+        public IActionResult SetSession()
+        {
+            Random rd = new Random();
+            int randNum = rd.Next(1, 100);
+            HttpContext.Session.SetInt32("randNumSession", randNum);
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SetSession(int numericInput)
+        {
+            
+
+            HttpContext.Session.SetInt32("numSession", numericInput);
+
+            if (numericInput == HttpContext.Session.GetInt32("randNumSession"))
+
+                ViewBag.Message = "Congratulations";
+
+            if (numericInput > HttpContext.Session.GetInt32("randNumSession"))
+
+                ViewBag.Message = "Hidden number is smaller";
+
+            if (numericInput < HttpContext.Session.GetInt32("randNumSession"))
+
+                ViewBag.Message = "Hidden number is greater";
+
+            return View();
+        }
+       
+        public IActionResult GetSession()
+        {
+            ViewBag.SessionNum = HttpContext.Session.GetInt32("numSession");
+
+            return View();
+        }
+
+       
+        
+           
+
+        
+        }
+}
